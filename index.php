2a871f8f3717374234830fcd4e238303e1974ea6
reachmazharul <reachmazharul@gmail.com>
2021-05-16 17:02:12 +0600

    Started

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    /*function myFunctionName(){
+        echo 2+2;
+    }
+    myFunctionName();*/
+
+    /*function greet($color, $name){
+        echo "<p>My name is $color and my color is $name</p>";
+    }
+    greet("red", "omuk");*/
+    
+?>
