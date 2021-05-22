877cf52cc3db32db864b3ad065508a9503dc00e6
reachmazharul <reachmazharul@gmail.com>
2021-05-22 21:32:41 +0600

    Event Custom Post Type

--- functions.php
+++ functions.php
@@ -18,3 +18,24 @@ function ficuni_after_suptheme_info(){
 }
 
 add_action('after_setup_theme','ficuni_after_suptheme_info');
+
+/* Event Post Type */
+
+function funiversity_eventposttype(){
+
+    register_post_type('event', array(
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar-alt'
+    ));
+
+}
+add_action('init', 'funiversity_eventposttype');
